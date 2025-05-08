f62f4d0dfc3146b05b952d8b9860ea4d663531b2
tropotek <info@tropotek.com>
2025-05-08 13:49:25 +1000

    ~Auto: Commit

--- src/Tbx/Console/Test.php
+++ src/Tbx/Console/Test.php
@@ -29,6 +29,9 @@ class Test extends Iface
             return Command::FAILURE;
         }
 
+        $projectPath = rtrim((string)getcwd(), '/');
+        vd(getcwd(), );
+
 //        $options = $input->getOptions();
 //        $arguments = $input->getArguments();
 //        $iniOptions = $input->getIniOptions();
